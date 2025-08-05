600203433ee404069391523b3ac9dca3abe1050f
zeiflonte <zeiflonte@gmail.com>
2025-08-05 19:13:48 +0400

    Fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/zoom/', // Set base path to match Traefik/Docker routing
 })
